cdac2c4483b0a0023b42493ac0f2140ad2c0fa1d
SathishSat <satsathish10@gmail.com>
2025-09-10 22:27:32 +0530

    Angular Routing Setup

--- NudgeMeWeb/src/app/app.ts
+++ NudgeMeWeb/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReminderComponent } from './component/remainder';
+import { ReminderService } from './service/remainder.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  providers: [ReminderService],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
